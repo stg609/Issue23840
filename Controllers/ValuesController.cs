fd6b5197892a1f52d8f66fdc932dd5a6348af329
Charlie <stg609@outlook.com>
2020-07-10 13:43:32 +0800

    Upload Sample project to reproduce the issue

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication34.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] { "aa" };
+        }
+
+        [HttpPost]
+        [RequestSizeLimit(10000)]
+        public ActionResult Post(IFormFile video)
+        {
+            return Ok();
+        }
+    }
+}
